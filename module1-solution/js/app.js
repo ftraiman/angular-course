fea907dd0f66d054b90d4fba7e775e107a4a4a87
Federico Traiman <ftraiman3@hotmail.com>
2016-10-26 10:19:53 -0300

    Almost Ready

---
+++ module1-solution/js/app.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope', '$filter'];
+
+  function LunchCheckController($scope, $filter) {
+    $scope.lunch = "";
+    $scope.message = "";
+    $scope.style = "";
+
+    $scope.lunchChecker = function () {
+
+      var foodArray = this.lunch.split(",");
+      var foodCounter = 0;
+
+      if(this.lunch.length == 0) {
+        this.message = "Please enter data first";
+        this.style = "red";
+        return 0;
+      }
+      for(var i=0; i < foodArray.length; i++){
+          var food = foodArray[i].replace(/^\s+/, '').replace(/\s+$/, '');
+        if(food === ''){
+          $scope.message = "NOT consider and empty item, i.e., , ,";
+          this.style = "red";
+          return 0;
+        } else {
+          foodCounter++;
+        }
+      }
+
+      if(foodCounter <= 3 )
+        this.message = "Enjoy!";
+      else
+        this.message = "Too much!";
+        this.style = "green";
+    };
+  }
+
+})();
