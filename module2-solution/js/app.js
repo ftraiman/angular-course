e4d853063ea1db432c29c10a38546beabe73717d
Federico Traiman <ftraiman3@gmail.com>
2016-11-04 18:51:29 -0300

    Casi pronto

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,72 +1,49 @@
 (function () {
     'use strict';
 
-    angular.module('ShoppingListApp', [])
-        .controller('ShoppingSellController', ShoppingSellController)
-        .controller('ShoppingBoughtController', ShoppingBoughtController)
-        .service('ShoppingListService', ShoppingListService);
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListService', ShoppingListCheckOffService);
 
-    ShoppingSellController.$inject = ['ShoppingListService'];
-    function ShoppingSellController(ShoppingListService) {
-        var sell = this;
+    ToBuyController.$inject = ['ShoppingListService'];
+    function ToBuyController(ShoppingListService) {
+        var toBuy = this;
 
-        sell.itemsToSell = ShoppingListService.itemsToSell;
-        sell.indexClicked;
+        toBuy.itemsToSell = ShoppingListService.itemsToSell;
 
-        sell.buy = function (index) {
-            console.log(index);
-            sell.indexClicked = index;
+        toBuy.buy = function (index) {
             ShoppingListService.buy(index);
         }
     }
 
+    AlreadyBoughtController.$inject = ['ShoppingListService'];
+    function AlreadyBoughtController(ShoppingListService) {
+        var ab = this;
 
-    ShoppingBoughtController.$inject = ['ShoppingListService'];
-    function ShoppingBoughtController(ShoppingListService) {
-        var itemSeller = this;
-        var showList = this;
-
-        showList.items = ShoppingListService.getItems();
-
-        showList.removeItem = function (itemIndex) {
-            ShoppingListService.removeItem(itemIndex);
-        };
+        ab.itemsBought = ShoppingListService.getItemsBought();
     }
 
-
-    function ShoppingListService() {
+    function ShoppingListCheckOffService() {
         var service = this;
 
-        service.itemsToSell =  [{ name: "Cookies", quantity: 10}, { name: "Milk", quantity: 2}, { name: "Bread", quantity: 5}, { name: "Butter", quantity: 4}, { name: "Spagetty", quantity: 8} ];
-        service.itemBought = [];
-        
+        service.itemsToSell = [{ name: "Cookies", quantity: 10 }, { name: "Milk", quantity: 2 }, { name: "Bread", quantity: 5 }, { name: "Butter", quantity: 4 }, { name: "Spagetty", quantity: 8 }];
+        service.itemsBought = [];
+
+
         service.buy = function (index) {
-        
-        var item = service.itemsToSell.slice(index,index + 1 );
-        
-        service.itemBought.push(item);
-        service.itemsToSell.splice(index, 1);
-        console.log(service.itemBought);
+            var item = service.itemsToSell.slice(index, index + 1);
+            service.itemsBought.push(item[0]);
+            service.itemsToSell.splice(index, 1);
         }
 
-        // List of shopping items
-        var items = [];
+        service.getItemsBought = function () {
+            return service.itemsBought;
+        }
 
-        service.addItem = function (itemName, quantity) {
-            var item = {
-                name: itemName,
-                quantity: quantity
-            };
-            items.push(item);
-        };
-
-        service.removeItem = function (itemIdex) {
-            items.splice(itemIdex, 1);
-        };
-
-        service.getItems = function () {
-            return items;
-        };
+        service.getItemsBoughtLenght = function () {
+            return service.itemsBought.lenght;
+        }
     }
 
 })();
